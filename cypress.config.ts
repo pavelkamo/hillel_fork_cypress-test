6ac278f2993cd475f943d4289730d5b5946865e2
Artem Shoiko <shoiko.a.s@gmail.com>
2023-12-20 14:07:58 +0200

    Add base URL

--- cypress.config.ts
+++ cypress.config.ts
@@ -40,7 +40,7 @@ export default defineConfig({
         }
       });
     },
-    // baseUrl: 'http://localhost:4200',
+    baseUrl: 'http://localhost:4200',
     specPattern: 'cypress/e2e/**/*.spec.{js,jsx,ts,tsx}'
   },
   screenshotOnRunFailure: true,   // default true
